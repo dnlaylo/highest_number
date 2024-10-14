e84cb26719d0fd940efa3adac8a99f8477130e74
dnlaylo <laylomarga@gmail.com>
2024-10-14 22:14:55 +0800

    If statement loop, reassignment of current highest number, and return value

--- find_highest.py
+++ find_highest.py
@@ -3,8 +3,12 @@ def find_highest(entries):
     # Assume that the first entry is the highest
     highest = entries[0]
     # A loop that compares each entry to "highest" using an if statement
+    for num in entries:
+        if num > highest:
             # Reassign the current highest number
+            highest = num
     # Return the highest number
+    return highest
 # Prompt the user to put their number entries
 # A list to store all entries
 # Call the method that finds the highest number
