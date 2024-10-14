af8c6c38e23bd8300a75477d700583395df6130f
dnlaylo <laylomarga@gmail.com>
2024-10-14 21:40:31 +0800

    Pseudocode

---
+++ find_highest.py
@@ -0,0 +1,9 @@
+# Method for finding the highest number
+    # Assume that the first entry is the highest
+    # A loop that compares each entry to "highest" using an if statement
+            # Reassign the current highest number
+    # Return the highest number
+# Prompt the user to put their number entries
+# A list to store all entries
+# Call the method that finds the highest number
+# Print the highest number
